06e007af72891d499892108249ac68d4854ffe2c
Michle99 <sammiyonyx@gmail.com>
2021-12-15 12:23:50 -0500

    commit -,

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [firstname, setFirstName] = useState('');
+  const [color, setBackgroundColor] = useState('');
+
+ const handleNameChange = e => {
+    // const firstname = e.target.value;
+    // this.setState( { firstname } );
+    setFirstName(e.target.value)
+  }
+
+  const changeColor = () =>setBackgroundColor("#142634");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(firstname);
+    alert(`Submitting Name ${firstname}`);
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{backgroundColor: color}}>
+
+      <h3 className="App-header">My First React App</h3>
+      <div className='App-container'>
+      <button onClick={changeColor} >
+        Change background color
+        </button>
+        <form onSubmit={handleSubmit}>
+          <fieldset>
+            <label>
+              First Name &nbsp;
+              <input 
+              firstname="firstname" 
+              value={firstname} 
+              onChange={handleNameChange} 
+              type="text" />
+            </label>
+          </fieldset>
+
+          <button type='submit'>Submit</button>
+        </form>
+        <strong>The entered name is: </strong><span>{ firstname}</span>
+      </div>
     </div>
   );
 }
