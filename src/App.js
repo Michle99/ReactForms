4ffa5a4d7fc3ac2f615f362183d9c7c5a5ef9d39
Michle99 <sammiyonyx@gmail.com>
2021-12-15 15:34:23 -0500

    commit -m

--- src/App.js
+++ src/App.js
@@ -4,22 +4,55 @@ import './App.css';
 function App() {
 
   const [firstname, setFirstName] = useState('');
+  const [lastname, setLastName] = useState('');
+  const [gender, setGender] = useState('');
+  const [fruit, setFruit] = useState([]);
+  const [checked, setChecked] = useState('');
+  const [comments, setComments] = useState('Enter your comments here!');
   const [color, setBackgroundColor] = useState('');
 
- const handleNameChange = e => {
+ const handlFirstNameChange = e => {
     // const firstname = e.target.value;
     // this.setState( { firstname } );
     setFirstName(e.target.value)
   }
+  const handleLastNameChange = e => {
+    setLastName(e.target.value)
+  }
 
-  const changeColor = () =>setBackgroundColor("#142634");
+  const handleMaleGender = e => {
+    setGender("male");
+  }
+  const handleFemaleGender = e => {
+    setGender("female");
+  }
+
+  const handleFalseCheckBox = e => {
+    setChecked("false");
+  }
+  const handleTrueCheckBox = e => {
+    setChecked("true");
+  }
+  const handleFruit = e => {
+    setFruit(e.target.value);
+  }
+  const handleComments = e => {
+    setComments(e.target.value);
+  }
+
+  const changeColor = () =>setBackgroundColor("lightblue");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(firstname);
-    alert(`Submitting Name ${firstname}`);
+    console.log(firstname + " " +lastname);
+    alert(`Submitting Name ${firstname+" "+lastname+" "+gender+" "+fruit+" "+checked+" "+comments}`);
     
   }
+
+  //option values for the select tag
+  let array = ["apple","banana","mango","strawberry"]
+  let options = array.map( (item) =>
+      <option value = {item}>{item}</option> )
 
   return (
     <div className="App" style={{backgroundColor: color}}>
@@ -36,14 +69,90 @@ function App() {
               <input 
               firstname="firstname" 
               value={firstname} 
-              onChange={handleNameChange} 
+              onChange={handlFirstNameChange} 
               type="text" />
             </label>
           </fieldset>
+          <fieldset>
+            <label>
+              Last Name &nbsp;
+              <input 
+              lastname="lastname" 
+              value={lastname} 
+              onChange={handleLastNameChange} 
+              type="text" />
+            </label>
+          </fieldset>
+          <fieldset>
+            <label>Gender &nbsp;&nbsp;&nbsp;
+            <input
+              type="radio"
+              name="gender"
+              value={gender}
+              checked={gender === "male"}
+              onClick={handleMaleGender}
+            /> Male
+            </label>
+            <label> &nbsp;
+            <input
+              type="radio"
+              name="gender"
+              value={gender}
+              checked={gender === "female"}
+              onClick={handleFemaleGender}
+            /> Female
+            </label>
+          </fieldset>
+          <fieldset>
+            <label>Pick your favorite fruit: &nbsp;
+            <select 
+              value={fruit}
+              onChange={handleFruit}
+              >
+                {options}
+            {/* <option selected value="apple">apple</option>
+            <option value="banana">banana</option>
+            <option value="mango">mango</option>
+            <option value="strawberry">strawberry</option> */}
+            </select>
+            </label>
+          </fieldset>
+          <fieldset>
+            <label>Are the above inputs correct?: &nbsp;
+            <input 
+              type="checkbox"
+              name="checked"
+              value={checked}
+              checked={checked === "true"}
+              onClick={handleTrueCheckBox}
+            />&nbsp;True &nbsp;
+             <input 
+              type="checkbox"
+              name="checked"
+              value={checked}
+              checked={checked === "false"}
+              onClick={handleFalseCheckBox}
+            />&nbsp;False
+            </label>
+          </fieldset>
+          <fieldset>
+            <label>Comments : &nbsp;
+            <textarea 
+              value={comments}
+                onInput={handleComments}
+              />
+            </label>
+          </fieldset>
+
 
           <button type='submit'>Submit</button>
         </form>
-        <strong>The entered name is: </strong><span>{ firstname}</span>
+        <strong>The entered first name is: </strong><span>{ firstname}</span><br/>
+        <strong>The entered last name is: </strong><span>{lastname}</span><br/>
+        <strong>The entered gender is: </strong><span>{gender}</span><br/>
+        <strong>The entered fruit is: </strong><span>{fruit}</span><br/>
+        <strong>The entered inputs are: </strong><span>{checked}</span><br/>
+        <strong>The entered cooments are: </strong><span>{comments}</span><br/>
       </div>
     </div>
   );
